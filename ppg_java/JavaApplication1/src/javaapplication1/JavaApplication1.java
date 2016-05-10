b2f59faa82c505486ddfb1625410ce0f793ca9df
ashraful <asrafulashiq@gmail.com>
2016-05-10 23:42:02 +0600

    some added

--- ppg_java/JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ ppg_java/JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -8,6 +8,7 @@ package javaapplication1;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -16,8 +17,8 @@ import java.util.Scanner;
  */
 public class JavaApplication1 {
     
-    public int fSampling = 125;
-    public int multiplier = fSampling/125;
+   
+    
 
     /**
      * @param args the command line arguments
@@ -25,6 +26,9 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) throws FileNotFoundException {
         // TODO code application logic here
+        
+        int fSampling = 125;
+        int multiplier = fSampling/125;
         
         ArrayList<Double> ecgSignal = getArray("DATA01_1.dat");
         ArrayList<Double> ppgSignal1 = getArray("DATA01_2.dat");
@@ -35,14 +39,92 @@ public class JavaApplication1 {
         
         ArrayList<Double> ppgSignalAverage = addTwoList(ppgSignal1,ppgSignal2);
         
+        /**
+         * RLS filtering 
+         */
+        int lParameterOfRLS = 40;
+        
+        ArrayList<Double> ex = rlsFilter(lParameterOfRLS, accDataX, ppgSignalAverage);
+        
+        ArrayList<Double> exy = rlsFilter(lParameterOfRLS, accDataY, ex);
+        ArrayList<Double> rRaw = rlsFilter(lParameterOfRLS, accDataZ, exy);
+     
+        
+        /**
+         * filtering all data to frequency range for human HR
+         */
+        ArrayList<Double> rN = myBandPass(rRaw, fSampling);
+        ArrayList<Double> accDataXfiltered = myBandPass(accDataX, fSampling);
+        ArrayList<Double> accDataYfiltered = myBandPass(accDataY, fSampling);
+        ArrayList<Double> accDataZfiltered = myBandPass(accDataZ, fSampling);
+        
+        float fPrev = initialize( rN.subList(0,1000), accDataX.subList(0,1000),accDataY.subList(0,1000),
+                accDataZ.subList(0, 1000),fSampling);
+        
+        
         
         
         
     }
     
+    
+    
+    
+    /**
+     * TODO
+     * @param sig
+     * @param accX
+     * @param accY
+     * @param accZ
+     * @param fSampling
+     * @return 
+     */
+    
+    public static float initialize(List<Double> sig, List<Double> accX, List<Double> accY,
+            List<Double> accZ, float fSampling){
+        
+        return 0;
+    }
+    
+    
+    //TODO
+    /**
+     * 
+     * @param rSig signal to be filtered
+     * @param fSampling sampling frequency
+     * @return 
+     */
+    public static ArrayList<Double> myBandPass(ArrayList<Double> rSig, double fSampling){
+        
+        return null;
+        
+    }
+    
+    
+    // TODO:
+    /**
+     * 
+     * @param lParameter parameter of rls filter
+     * @param hSig helper signal
+     * @param mainSig main signal
+     * @return filtered signal
+     */
+    
+    public static ArrayList<Double> rlsFilter(int lParameter, ArrayList<Double> hSig, ArrayList<Double> mainSig){
+        
+        return null;
+    }
+    
+    /**
+     * 
+     * @param l1 arrayList 1
+     * @param l2 arrayList 2
+     * @return arrayList 
+     */
     public static ArrayList<Double> addTwoList(ArrayList<Double> l1,ArrayList<Double> l2){
         int len = l1.size();
-        ArrayList<Double> l3 = new ArrayList<>();
+        ArrayList<Double> l3;
+        l3 = new ArrayList<>();
         for(int i=0;i<len;i++){
             l3.add( l1.get(i) + l2.get(i) );
         }
--- ppg_java/JavaApplication1/test/javaapplication1/JavaApplication1Test.java
+++ ppg_java/JavaApplication1/test/javaapplication1/JavaApplication1Test.java
@@ -6,13 +6,16 @@
 package javaapplication1;
 
 import java.util.ArrayList;
+import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-
+import static org.junit.Assert.fail;
 /**
  *
  * @author mac
@@ -50,6 +53,94 @@ public class JavaApplication1Test {
         //fail("The test case is a prototype.");
     }
 
+    
+    /**
+     * 
+     */
+    @Test
+    public  void testAddTwoList(){
+        ArrayList<Double> a1 = new ArrayList<>();
+        a1.add(Double.valueOf(5));
+        a1.add(Double.valueOf(3));
+        
+        ArrayList<Double> a2 = new ArrayList<>();
+        a2.add(Double.valueOf(-2));
+        a2.add(Double.valueOf(0));
+        
+        ArrayList<Double> a3 = JavaApplication1.addTwoList(a1, a2);
+        
+        ArrayList<Double> expected = new ArrayList<>();
+        expected.add(Double.valueOf(3));
+        expected.add(Double.valueOf(3));
+        
+        assertTrue(expected.equals(a3));
+        
+        
+    }
+
+    /**
+     * Test of rlsFilter method, of class JavaApplication1.
+     */
+    @Test
+    public void testRlsFilter() {
+        System.out.println("rlsFilter");
+        int lParameter = 0;
+        ArrayList<Double> hSig = null;
+        ArrayList<Double> mainSig = null;
+        JavaApplication1 instance = new JavaApplication1();
+        ArrayList<Double> expResult = null;
+        ArrayList<Double> result = instance.rlsFilter(lParameter, hSig, mainSig);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getArray method, of class JavaApplication1.
+     */
+    @Test
+    public void testGetArray() throws Exception {
+        System.out.println("getArray");
+        String fileName = "";
+        ArrayList<Double> expResult = null;
+        ArrayList<Double> result = JavaApplication1.getArray(fileName);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of myBandPass method, of class JavaApplication1.
+     */
+    @Test
+    public void testMyBandPass() {
+        System.out.println("myBandPass");
+        ArrayList<Double> rSig = null;
+        double fSampling = 0.0;
+        ArrayList<Double> expResult = null;
+        ArrayList<Double> result = JavaApplication1.myBandPass(rSig, fSampling);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of initialize method, of class JavaApplication1.
+     */
+    @Test
+    public void testInitialize() {
+        System.out.println("initialize");
+        List<Double> sig = null;
+        List<Double> accX = null;
+        List<Double> accY = null;
+        List<Double> accZ = null;
+        float fSampling = 0.0F;
+        float expResult = 0.0F;
+        float result = JavaApplication1.initialize(sig, accX, accY, accZ, fSampling);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
     
     
 }
